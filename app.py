4500903c8ea5ecc6577947ade75cdfd34a4476c9
UnderAnder <andryushin.alexander@gmail.com>
2021-03-12 23:47:16 +0700

    flask boilerplate

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+import sys
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Hello, world!'
+
+
+@app.route('/profile')
+def profile():
+    return 'This is profile page'
+
+
+@app.route('/login')
+def log_in():
+    return 'This is login page'
+
+
+# don't change the following way to run flask:
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        arg_host, arg_port = sys.argv[1].split(':')
+        app.run(host=arg_host, port=arg_port)
+    else:
+        app.run()
